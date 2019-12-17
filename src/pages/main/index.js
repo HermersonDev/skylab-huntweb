5a9a0058ca7c490893590c77785ec5660a03ee87
HermersonDev <ne.hermerson@gmail.com>
2019-12-16 23:11:05 -0300

    Create loadProduct method

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -2,6 +2,16 @@ import React, { Component } from "react";
 import api from "../../services/api";
 
 class Main extends Component {
+	
+	componentDidMount() {
+		this.loadProducts();
+	}
+
+	loadProducts = async () => {
+		let products = await api.get('/products');
+		console.log(products);
+	}
+
 	render() {
 		return <h1>Main Page</h1>;
 	}
