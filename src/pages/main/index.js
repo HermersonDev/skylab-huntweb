7adb8d7dced4177d5110725bc3ed486b2cb2f5d9
HermersonDev <ne.hermerson@gmail.com>
2019-12-16 23:05:02 -0300

    Add Main page

---
+++ src/pages/main/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import api from "../../services/api";
+
+class Main extends Component {
+	render() {
+		return <h1>Main Page</h1>;
+	}
+}
+
+export default Main;
