a08511d496810eb1f124fd09f9da854dfb97e84e
HermersonDev <ne.hermerson@gmail.com>
2019-12-17 20:29:20 -0300

    Listing products

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -2,18 +2,34 @@ import React, { Component } from "react";
 import api from "../../services/api";
 
 class Main extends Component {
-	
+	state = {
+		products: []
+	}
+
 	componentDidMount() {
 		this.loadProducts();
 	}
 
 	loadProducts = async () => {
-		const products = await api.get('/products');
-		console.log(products);
-	}
+		const response = await api.get('/products');
+		console.log(response);
+		this.setState({ products: response.data.docs });
+	};
 
 	render() {
-		return <h1>Main Page</h1>;
+		return (
+			<div>
+				<span id="products-count">
+					{ this.state.products.length } 
+				</span>
+
+				<div className="product-list">
+					{this.state.products.map(product => (
+						<h3 key={product._id}>{product.title}</h3>
+					))}				
+				</div>
+			</div>
+		);
 	}
 }
 
