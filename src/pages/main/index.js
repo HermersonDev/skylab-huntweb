adcdadcd93878e69dc1e48bff88d505acfd6d91b
HermersonDev <ne.hermerson@gmail.com>
2019-12-17 22:06:57 -0300

    Add actions buttons

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -4,22 +4,48 @@ import api from "../../services/api";
 import "./styles.css";
 
 class Main extends Component {
+	
 	state = {
-		products: []
+		products: [],
+		productInfo: [],
+		page: 1
 	}
 
 	componentDidMount() {
 		this.loadProducts();
 	}
 
-	loadProducts = async () => {
-		const response = await api.get('/products');
-		console.log(response);
-		this.setState({ products: response.data.docs });
-	};
+	loadProducts = async (page = 1) => {
+		const response = await api.get(`/products?page=${page}`);
+		const { docs, ...productInfo } = response.data;
+
+		this.setState({ 
+			products: docs, 
+			productInfo: productInfo,
+			page: page 
+		});
+	}
+
+	prevPage = () => {
+		const { page, productInfo } = this.state;
+
+		if (page === 1)
+			return;
+
+		this.loadProducts(page - 1);
+	}
+
+	nextPage = () => {
+		const { page, productInfo } = this.state;
+		
+		if (page === productInfo.pages)
+			return;
+
+		this.loadProducts(page + 1);
+	}
 
 	render() {
-		const { products } = this.state;
+		const { products, productInfo, page } = this.state;
 
 		return (
 			<div>
@@ -32,7 +58,17 @@ class Main extends Component {
 							<p>{product.description}</p>
 							<a href="">Acessar</a>
 						</article>
-					))}				
+					))}		
+					<div className="actions">
+						<button onClick={this.prevPage}
+								disabled={page === 1} >
+							Anterior
+						</button>
+						<button onClick={this.nextPage}
+								disabled={page === productInfo.pages} >
+							Próximo
+						</button>
+					</div>		
 				</div>
 			</div>
 		);
